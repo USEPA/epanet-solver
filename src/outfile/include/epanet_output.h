cbca89de19637d683e4518eb2799ff4ac1db9308
Michael Tryby <tryby.michael@epa.gov>
2019-08-19 17:25:33 -0400

    Update epanet_output.h
    Work in progress

--- src/outfile/include/epanet_output.h
+++ src/outfile/include/epanet_output.h
@@ -54,7 +54,6 @@ int EXPORT_OUT_API ENR_open(ENR_Handle p_handle, const char* path);
 @param p_handle an EPANET output handle
 @param[out] int_out the EPANET version number found in the output file prologue.
 @return an error code
-
 */
 int EXPORT_OUT_API ENR_getVersion(ENR_Handle p_handle, int* int_out);
 
@@ -86,7 +85,6 @@ int EXPORT_OUT_API ENR_getNetSize(ENR_Handle p_handle, int** int_out, int* int_d
 @param[out] int_out the flag value for the unit type (see @ref ENR_FlowUnits,
 @ref ENR_PressUnits, @ref ENR_QualUnits)
 @return an error code
-
 */
 int EXPORT_OUT_API ENR_getUnits(ENR_Handle p_handle, ENR_Units t_enum, int* int_out);
 
@@ -97,7 +95,6 @@ int EXPORT_OUT_API ENR_getUnits(ENR_Handle p_handle, ENR_Units t_enum, int* int_
 @param t_enum a time parameter code (see @ref ENR_Time).
 @param[out] int_out the value of the time parameter.
 @return an error code
-
 */
 int EXPORT_OUT_API ENR_getTimes(ENR_Handle p_handle, ENR_Time t_enum, int* int_out);
 
@@ -110,10 +107,9 @@ int EXPORT_OUT_API ENR_getTimes(ENR_Handle p_handle, ENR_Time t_enum, int* int_o
 @param[out] string_out
 @param[out] slen
 @return an error code
-
 */
 int EXPORT_OUT_API ENR_getElementName(ENR_Handle p_handle, ENR_ElementType t_enum,
-        int elementIndex, char** string_out, int* slen);
+    int elementIndex, char** string_out, int* slen);
 
 
 /**
@@ -124,10 +120,9 @@ int EXPORT_OUT_API ENR_getElementName(ENR_Handle p_handle, ENR_ElementType t_enu
 @param float_out
 @param int_dim
 @return an error code
-
 */
 int EXPORT_OUT_API ENR_getEnergyUsage(ENR_Handle p_handle, int pumpIndex,
-        int* int_out, float** float_out, int* int_dim);
+    int* int_out, float** float_out, int* int_dim);
 
 
 /**
@@ -136,7 +131,6 @@ int EXPORT_OUT_API ENR_getEnergyUsage(ENR_Handle p_handle, int pumpIndex,
 @param float_out
 @param int_dim
 @return an error code
-
 */
 int EXPORT_OUT_API ENR_getNetReacts(ENR_Handle p_handle, float** float_out, int* int_dim);
 
@@ -144,96 +138,89 @@ int EXPORT_OUT_API ENR_getNetReacts(ENR_Handle p_handle, float** float_out, int*
 /**
 @brief Get time series results for particular node attribute.
 @param p_handle an EPANET output handle
-@param nodeIndex
-@param t_enum
-@param startPeriod
-@param endPeriod
-@param outValueSeries
-@param dim
+@param nodeIndex the node index for time series
+@param attr code for the node attribute to report (see @ref ENR_NodeAttribute)
+@param startPeriod time period of series start
+@param endPeriod time period of series end
+@param outValueSeries array of time series values
+@param length the number of elements in the outValueArray
 @return an error code
-
 */
-int EXPORT_OUT_API ENR_getNodeSeries(ENR_Handle p_handle, int nodeIndex, ENR_NodeAttribute t_enum,
-        int startPeriod, int endPeriod, float** outValueSeries, int* dim);
+int EXPORT_OUT_API ENR_getNodeSeries(ENR_Handle p_handle, int nodeIndex, ENR_NodeAttribute attr,
+    int startPeriod, int endPeriod, float** outValueSeries, int* length);
 
 
 /**
 @brief Get time series results for particular link attribute.
 @param p_handle an EPANET output handle
-@param linkIndex
-@param t_enum
-@param startPeriod
-@param endPeriod
-@param outValueSeries
-@param dim
+@param linkIndex the link index for time series
+@param attr code for the link attribute to report (see @ref ENR_LinkAttribute)
+@param startPeriod time period of series start
+@param endPeriod time period of series end
+@param outValueSeries array of time series values
+@param length the number of elements in the outValueArray
 @return an error code
-
 */
-int EXPORT_OUT_API ENR_getLinkSeries(ENR_Handle p_handle, int linkIndex, ENR_LinkAttribute t_enum,
-        int startPeriod, int endPeriod, float** outValueSeries, int* dim);
+int EXPORT_OUT_API ENR_getLinkSeries(ENR_Handle p_handle, int linkIndex, ENR_LinkAttribute attr,
+    int startPeriod, int endPeriod, float** outValueSeries, int* length);
 
 
 /**
 @brief Get a particular attribute for all nodes at given time.
 @param p_handle an EPANET output handle
-@param periodIndex
-@param t_enum
-@param outValueArray
-@param dim
+@param periodIndex the time period index for reporting results
+@param attr code for the node attribute to report (see @ref ENR_NodeAttribute)
+@param outValueArray the array of node attributes
+@param length the number of elements in the outValueArray
 @return an error code
-
 */
 int EXPORT_OUT_API ENR_getNodeAttribute(ENR_Handle p_handle, int periodIndex,
-        ENR_NodeAttribute t_enum, float** outValueArray, int* dim);
+    ENR_NodeAttribute attr, float** outValueArray, int* dim);
 
 
 /**
 @brief Get a particular attribute for all links at given time.
 @param p_handle an EPANET output handle
-@param periodIndex
-@param t_enum
-@param outValueSeries
-@param dim
+@param periodIndex the time period index for reporting results
+@param attr code for the link attribute to report (see @ref ENR_LinkAttribute)
+@param outValueArray the array of link attributes
+@param length the number of elements in the outValueArray
 @return an error code
-
 */
 int EXPORT_OUT_API ENR_getLinkAttribute(ENR_Handle p_handle, int periodIndex,
-        ENR_LinkAttribute t_enum, float** outValueArray, int* dim);
+    ENR_LinkAttribute attr, float** outValueArray, int* length);
 
 
 /**
 @brief Get all attributes for a node at given time.
 @param p_handle an EPANET output handle
-@param periodIndex
-@param nodeIndex
-@param float_out
-@param int_dim
+@param periodIndex the time period index for reporting results
+@param nodeIndex the node index for reporting results
+@param outValueArray the array of node attributes
+@param length the number of elements in the outValueArray
 @return an error code
-
 */
-int EXPORT_OUT_API ENR_getNodeResult(ENR_Handle p_handle, int periodIndex, int nodeIndex,
-        float** float_out, int* int_dim);
+int EXPORT_OUT_API ENR_getNodeResult(ENR_Handle p_handle, int periodIndex,
+    int nodeIndex, float** outValueArray, int* length);
 
 
 /**
 @brief Get all attributes for a link at given time.
 @param p_handle an EPANET output handle
-@param periodIndex
-@param linkIndex
-@param float_out
-@param int_dim
+@param periodIndex the time period index for reporting results
+@param linkIndex the link index for reporting results
+@param outValueArray the array of link attributes
+@param length the number of elements in the outValueArray
 @return an error code
-
 */
-int EXPORT_OUT_API ENR_getLinkResult(ENR_Handle p_handle, int periodIndex, int linkIndex,
-        float** float_out, int* int_dim);
+int EXPORT_OUT_API ENR_getLinkResult(ENR_Handle p_handle, int periodIndex,
+    int linkIndex, float** outValueArray, int* length);
 
 
 /**
 @brief Close the output file and destroy the handle.
 @param p_handle an EPANET output handle
 @return an error code -- 0 on success or -1 on failure
-
 */
 int EXPORT_OUT_API ENR_close(ENR_Handle* p_handle);
 
@@ -242,7 +229,6 @@ int EXPORT_OUT_API ENR_close(ENR_Handle* p_handle);
 @brief Free memory allocated by API call.
 @param array the memory to be freed.
 @return an error code
-
 */
 void EXPORT_OUT_API ENR_free(void** array);
 
@@ -250,7 +236,6 @@ void EXPORT_OUT_API ENR_free(void** array);
 /**
 @brief Clears the current error status.
 @param p_handle an EPANET output handle
-
 */
 void EXPORT_OUT_API ENR_clearError(ENR_Handle p_handle);
 
@@ -260,7 +245,6 @@ void EXPORT_OUT_API ENR_clearError(ENR_Handle p_handle);
 @param p_handle an EPANET output handle
 @param[out] msg_buffer contains the message associated with the current error code.
 @return the current error code
-
 */
 int EXPORT_OUT_API ENR_checkError(ENR_Handle p_handle, char** msg_buffer);
 
