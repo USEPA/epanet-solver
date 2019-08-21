325ed2176b8138fbb46f126db268413dfa8f04b3
Michael Tryby <tryby.michael@epa.gov>
2019-08-21 17:36:09 -0400

    Work in progress
    Adding docs
    Minor refactoring

--- src/outfile/include/epanet_output.h
+++ src/outfile/include/epanet_output.h
@@ -2,9 +2,10 @@
  *  epanet_output.h - EPANET Output API
  *
  *  Created on: Jun 4, 2014
+ *  Modified on August 21, 2018
  *
- *      Author: Michael E. Tryby
- *              US EPA - ORD/NRMRL
+ *  Author: Michael E. Tryby
+ *          US EPA - ORD/NRMRL
  */
 
 #ifndef EPANET_OUTPUT_H_
@@ -19,7 +20,7 @@
 /**
 @brief EPANET Output file handle
 */
-typedef void* ENR_Handle;
+typedef void *ENR_Handle;
 
 
 #include "epanet_output_enums.h"
@@ -37,7 +38,7 @@ all other Output API functions.
 @return an error code -- 0 on success or -1 on failure
 
 */
-int EXPORT_OUT_API ENR_init(ENR_Handle* p_handle);
+int EXPORT_OUT_API ENR_init(ENR_Handle *p_handle);
 
 
 /**
@@ -46,23 +47,23 @@ int EXPORT_OUT_API ENR_init(ENR_Handle* p_handle);
 @param path the name of the binary output file to be opened.
 @return an error code
 */
-int EXPORT_OUT_API ENR_open(ENR_Handle p_handle, const char* path);
+int EXPORT_OUT_API ENR_open(ENR_Handle p_handle, const char *path);
 
 
 /**
 @brief Get the EPANET version that produced the output file.
 @param p_handle an EPANET output handle
-@param[out] int_out the EPANET version number found in the output file prologue.
+@param[out] version the EPANET version number found in the output file prologue.
 @return an error code
 */
-int EXPORT_OUT_API ENR_getVersion(ENR_Handle p_handle, int* int_out);
+int EXPORT_OUT_API ENR_getVersion(ENR_Handle p_handle, int *version);
 
 
 /**
 @brief Get an array of element count values.
 @param p_handle an EPANET output handle
-@param[out] int_out array of element count values
-@param[out] int_dim array size
+@param[out] count array of element count values
+@param[out] size array size
 @return an error code
 
 Element count array contents:
@@ -75,94 +76,123 @@ Element count array contents:
 This function allocates memory for the element count array. The caller is
 responsible for freeing it using ::ENR_free.
 */
-int EXPORT_OUT_API ENR_getNetSize(ENR_Handle p_handle, int** int_out, int* int_dim);
+int EXPORT_OUT_API ENR_getNetSize(ENR_Handle p_handle, int **count, int *size);
 
 
 /**
 @brief Get unit flag for pressure, flow, and quality.
 @param p_handle an EPANET output handle
-@param t_enum a unit type code (see @ref ENR_Units)
-@param[out] int_out the flag value for the unit type (see @ref ENR_FlowUnits,
+@param code a unit type code (see @ref ENR_Units)
+@param[out] flag the flag value for the unit type (see @ref ENR_FlowUnits,
 @ref ENR_PressUnits, @ref ENR_QualUnits)
 @return an error code
 */
-int EXPORT_OUT_API ENR_getUnits(ENR_Handle p_handle, ENR_Units t_enum, int* int_out);
+int EXPORT_OUT_API ENR_getUnits(ENR_Handle p_handle, ENR_Units code, int *flag);
 
 
 /**
 @brief Get report and simulation time related parameters.
 @param p_handle an EPANET output handle
-@param t_enum a time parameter code (see @ref ENR_Time).
-@param[out] int_out the value of the time parameter.
+@param code a time parameter code (see @ref ENR_Time).
+@param[out] value the value of the time parameter.
 @return an error code
 */
-int EXPORT_OUT_API ENR_getTimes(ENR_Handle p_handle, ENR_Time t_enum, int* int_out);
+int EXPORT_OUT_API ENR_getTimes(ENR_Handle p_handle, ENR_Time code, int *value);
 
 
 /**
 @brief Retrieves name of a specified node or link element.
 @param p_handle an EPANET output handle
-@param t_enum
-@param elementIndex
-@param[out] string_out
-@param[out] slen
+@param type the type of the element being queried
+@param index the index of the element being queried
+@param[out] name the name of the element
+@param[out] length the length of the element name
 @return an error code
+
+This function allocates memory for the element name string. The caller is
+responsible for freeing it using ::ENR_free.
 */
-int EXPORT_OUT_API ENR_getElementName(ENR_Handle p_handle, ENR_ElementType t_enum,
-    int elementIndex, char** string_out, int* slen);
+int EXPORT_OUT_API ENR_getElementName(ENR_Handle p_handle, ENR_ElementType type,
+    int index, char **name, int *length);
 
 
 /**
 @brief Returns pump energy usage statistics.
 @param p_handle an EPANET output handle
-@param pumpIndex
-@param int_out
-@param float_out
-@param int_dim
+@param pumpIndex the index of the pump being queried
+@param linkIndex the link index associated with the pump
+@param values an array of energy usage values
+@param size the size of the energy values array
 @return an error code
+
+Energy usage statistics:
+  0. pump utilization
+  1. avg. efficiency
+  2. avg. kW/flow
+  3. avg. kwatts
+  4. peak kwatts
+  5. cost/day
+
+This function allocates memory for the energy usage array. The caller is
+responsible for freeing it using ::ENR_free.
 */
 int EXPORT_OUT_API ENR_getEnergyUsage(ENR_Handle p_handle, int pumpIndex,
-    int* int_out, float** float_out, int* int_dim);
+    int *linkIndex, float **values, int *size);
 
 
 /**
-@brief Returns network average reaction rates and average source mass inflow.
+@brief Get network average reaction rates and average source mass inflow.
 @param p_handle an EPANET output handle
-@param float_out
-@param int_dim
+@param values an array of network reaction values
+@param size the size of the network reaction array
 @return an error code
+
+Average reaction rates and souce mass values:
+  0. bulk
+  1. wall
+  2. tank
+  3. source
+
+This function allocates memory for the network reaction array. The caller is
+responsible for freeing it using ::ENR_free.
 */
-int EXPORT_OUT_API ENR_getNetReacts(ENR_Handle p_handle, float** float_out, int* int_dim);
+int EXPORT_OUT_API ENR_getNetReacts(ENR_Handle p_handle, float **values, int *size);
 
 
 /**
 @brief Get time series results for particular node attribute.
 @param p_handle an EPANET output handle
-@param nodeIndex the node index for time series
+@param index the node index for time series
 @param attr code for the node attribute to report (see @ref ENR_NodeAttribute)
 @param startPeriod time period of series start
 @param endPeriod time period of series end
-@param outValueSeries array of time series values
-@param length the number of elements in the outValueArray
+@param series array of time series values
+@param length of the time series
 @return an error code
+
+This function allocates memory for the node attribute series. The caller is
+responsible for freeing it using ::ENR_free.
 */
-int EXPORT_OUT_API ENR_getNodeSeries(ENR_Handle p_handle, int nodeIndex, ENR_NodeAttribute attr,
-    int startPeriod, int endPeriod, float** outValueSeries, int* length);
+int EXPORT_OUT_API ENR_getNodeSeries(ENR_Handle p_handle, int index, ENR_NodeAttribute attr,
+    int startPeriod, int endPeriod, float **series, int *length);
 
 
 /**
 @brief Get time series results for particular link attribute.
 @param p_handle an EPANET output handle
-@param linkIndex the link index for time series
+@param index the link index for time series
 @param attr code for the link attribute to report (see @ref ENR_LinkAttribute)
 @param startPeriod time period of series start
 @param endPeriod time period of series end
-@param outValueSeries array of time series values
-@param length the number of elements in the outValueArray
+@param series array of time series values
+@param length the time series
 @return an error code
+
+This function allocates memory for the link attribute series. The caller is
+responsible for freeing it using ::ENR_free.
 */
-int EXPORT_OUT_API ENR_getLinkSeries(ENR_Handle p_handle, int linkIndex, ENR_LinkAttribute attr,
-    int startPeriod, int endPeriod, float** outValueSeries, int* length);
+int EXPORT_OUT_API ENR_getLinkSeries(ENR_Handle p_handle, int index, ENR_LinkAttribute attr,
+    int startPeriod, int endPeriod, float **series, int *length);
 
 
 /**
@@ -170,12 +200,15 @@ int EXPORT_OUT_API ENR_getLinkSeries(ENR_Handle p_handle, int linkIndex, ENR_Lin
 @param p_handle an EPANET output handle
 @param periodIndex the time period index for reporting results
 @param attr code for the node attribute to report (see @ref ENR_NodeAttribute)
-@param outValueArray the array of node attributes
-@param length the number of elements in the outValueArray
+@param values the array of node attribute values
+@param size the size of the node attribute array
 @return an error code
+
+This function allocates memory for the node attribute array. The caller is
+responsible for freeing it using ::ENR_free.
 */
 int EXPORT_OUT_API ENR_getNodeAttribute(ENR_Handle p_handle, int periodIndex,
-    ENR_NodeAttribute attr, float** outValueArray, int* dim);
+    ENR_NodeAttribute attr, float **values, int *size);
 
 
 /**
@@ -183,12 +216,15 @@ int EXPORT_OUT_API ENR_getNodeAttribute(ENR_Handle p_handle, int periodIndex,
 @param p_handle an EPANET output handle
 @param periodIndex the time period index for reporting results
 @param attr code for the link attribute to report (see @ref ENR_LinkAttribute)
-@param outValueArray the array of link attributes
-@param length the number of elements in the outValueArray
+@param values the array of link attribute values
+@param size the size of the link attribute array
 @return an error code
+
+This function allocates memory for the link attribute array. The caller is
+responsible for freeing it using ::ENR_free.
 */
 int EXPORT_OUT_API ENR_getLinkAttribute(ENR_Handle p_handle, int periodIndex,
-    ENR_LinkAttribute attr, float** outValueArray, int* length);
+    ENR_LinkAttribute attr, float **values, int *size);
 
 
 /**
@@ -196,12 +232,15 @@ int EXPORT_OUT_API ENR_getLinkAttribute(ENR_Handle p_handle, int periodIndex,
 @param p_handle an EPANET output handle
 @param periodIndex the time period index for reporting results
 @param nodeIndex the node index for reporting results
-@param outValueArray the array of node attributes
-@param length the number of elements in the outValueArray
+@param values the array of node attributes
+@param size the size of the values array
 @return an error code
+
+This function allocates memory for the node result array. The caller is
+responsible for freeing it using ::ENR_free.
 */
 int EXPORT_OUT_API ENR_getNodeResult(ENR_Handle p_handle, int periodIndex,
-    int nodeIndex, float** outValueArray, int* length);
+    int nodeIndex, float **values, int *size);
 
 
 /**
@@ -209,12 +248,15 @@ int EXPORT_OUT_API ENR_getNodeResult(ENR_Handle p_handle, int periodIndex,
 @param p_handle an EPANET output handle
 @param periodIndex the time period index for reporting results
 @param linkIndex the link index for reporting results
-@param outValueArray the array of link attributes
-@param length the number of elements in the outValueArray
+@param values the array of link attribute values
+@param size the size of the values array
 @return an error code
+
+This function allocates memory for the link result array. The caller is
+responsible for freeing it using ::ENR_free.
 */
 int EXPORT_OUT_API ENR_getLinkResult(ENR_Handle p_handle, int periodIndex,
-    int linkIndex, float** outValueArray, int* length);
+    int linkIndex, float **values, int *size);
 
 
 /**
@@ -222,7 +264,7 @@ int EXPORT_OUT_API ENR_getLinkResult(ENR_Handle p_handle, int periodIndex,
 @param p_handle an EPANET output handle
 @return an error code -- 0 on success or -1 on failure
 */
-int EXPORT_OUT_API ENR_close(ENR_Handle* p_handle);
+int EXPORT_OUT_API ENR_close(ENR_Handle *p_handle);
 
 
 /**
@@ -230,7 +272,7 @@ int EXPORT_OUT_API ENR_close(ENR_Handle* p_handle);
 @param array the memory to be freed.
 @return an error code
 */
-void EXPORT_OUT_API ENR_free(void** array);
+void EXPORT_OUT_API ENR_free(void **array);
 
 
 /**
@@ -245,8 +287,11 @@ void EXPORT_OUT_API ENR_clearError(ENR_Handle p_handle);
 @param p_handle an EPANET output handle
 @param[out] msg_buffer contains the message associated with the current error code.
 @return the current error code
+
+This function allocates memory for the message buffer. The caller is
+responsible for freeing it using ::ENR_free.
 */
-int EXPORT_OUT_API ENR_checkError(ENR_Handle p_handle, char** msg_buffer);
+int EXPORT_OUT_API ENR_checkError(ENR_Handle p_handle, char **msg_buffer);
 
 
 #ifdef __cplusplus
--- src/outfile/include/epanet_output_enums.h
+++ src/outfile/include/epanet_output_enums.h
@@ -11,68 +11,91 @@
 #ifndef EPANET_OUTPUT_ENUMS_H_
 #define EPANET_OUTPUT_ENUMS_H_
 
-
+/**
+ Network element types
+*/
 typedef enum {
-    ENR_node        = 1,
-    ENR_link        = 2
+    ENR_node        = 1,  //!< Nodes
+    ENR_link        = 2   //!< Links
 } ENR_ElementType;
 
+/**
+Unit types
+*/
 typedef enum {
-    ENR_flowUnits   = 1,
-    ENR_pressUnits  = 2,
-    ENR_qualUnits   = 3
+    ENR_flowUnits   = 1,  //!< Flow units
+    ENR_pressUnits  = 2,  //!< Pressure units
+    ENR_qualUnits   = 3   //!< Quality units
 } ENR_Units;
 
+/**
+Flow unit types
+*/
 typedef enum {
-    ENR_CFS         = 0,
-    ENR_GPM         = 1,
-    ENR_MGD         = 2,
-    ENR_IMGD        = 3,
-    ENR_AFD         = 4,
-    ENR_LPS         = 5,
-    ENR_LPM         = 6,
-    ENR_MLD         = 7,
-    ENR_CMH         = 8,
-    ENR_CMD         = 9
-} ENR_FlowUnits;
+  EN_CFS            = 0,  //!< Cubic feet per second
+  EN_GPM            = 1,  //!< Gallons per minute
+  EN_MGD            = 2,  //!< Million gallons per day
+  EN_IMGD           = 3,  //!< Imperial million gallons per day
+  EN_AFD            = 4,  //!< Acre-feet per day
+  EN_LPS            = 5,  //!< Liters per second
+  EN_LPM            = 6,  //!< Liters per minute
+  EN_MLD            = 7,  //!< Million liters per day
+  EN_CMH            = 8,  //!< Cubic meters per hour
+  EN_CMD            = 9   //!< Cubic meters per day
+} EN_FlowUnits;
 
+/**
+Pressure unit types
+*/
 typedef enum {
-	ENR_PSI         = 0,
-	ENR_MTR         = 1,
-	ENR_KPA         = 2
+	ENR_PSI         = 0,  //!< Pounds per square inch psi
+	ENR_MTR         = 1,  //!< Meters m
+	ENR_KPA         = 2   //!< Kilo Pascal kPa
 } ENR_PressUnits;
 
+/**
+Quality unit types
+*/
 typedef enum {
-    ENR_NONE        = 0,
-    ENR_MGL         = 1,
-    ENR_UGL         = 2,
-    ENR_HOURS       = 3,
-    ENR_PRCNT       = 4
+    ENR_NONE        = 0,  //!< None
+    ENR_MGL         = 1,  //!< Milli gram per liter mg/L
+    ENR_UGL         = 2,  //!< Micro gram per liter ug/L
+    ENR_HOURS       = 3,  //!< Hours hrs
+    ENR_PRCNT       = 4   //!< Percent %
 } ENR_QualUnits;
 
+/**
+Report and time types
+*/
 typedef enum {
-    ENR_reportStart = 1,
-    ENR_reportStep  = 2,
-    ENR_simDuration = 3,
-    ENR_numPeriods  = 4
+    ENR_reportStart = 1,  //!< Report start
+    ENR_reportStep  = 2,  //!< Report step
+    ENR_simDuration = 3,  //!< Simulation duration
+    ENR_numPeriods  = 4   //!< Number of time periods
 }ENR_Time;
 
+/**
+Node attribute types
+*/
 typedef enum {
-    ENR_demand      = 1,
-    ENR_head        = 2,
-    ENR_pressure    = 3,
-    ENR_quality     = 4
+    ENR_demand      = 1,  //!< Nodal demand
+    ENR_head        = 2,  //!< Nodal hydraulic head
+    ENR_pressure    = 3,  //!< Nodal pressure
+    ENR_quality     = 4   //!< Nodel quality
 } ENR_NodeAttribute;
 
+/**
+Link attribute types
+*/
 typedef enum {
-    ENR_flow        = 1,
-    ENR_velocity    = 2,
-    ENR_headloss    = 3,
-    ENR_avgQuality  = 4,
-    ENR_status      = 5,
-    ENR_setting     = 6,
-    ENR_rxRate      = 7,
-    ENR_frctnFctr   = 8
+    ENR_flow        = 1,  //!< Link flow rate
+    ENR_velocity    = 2,  //!< Link velocity
+    ENR_headloss    = 3,  //!< Link head loss
+    ENR_avgQuality  = 4,  //!< Link average quality
+    ENR_status      = 5,  //!< Link status
+    ENR_setting     = 6,  //!< Link setting
+    ENR_rxRate      = 7,  //!< Link reaction rate
+    ENR_frctnFctr   = 8   //!< Link friction factor
 } ENR_LinkAttribute;
 
 
